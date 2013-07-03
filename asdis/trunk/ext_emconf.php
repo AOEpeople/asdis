4ce98a3c679154c59d5a33246faf7e9856e4f6c6
tifx <tifx@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-03 13:33:07 +0000

    updated version

--- asdis/trunk/ext_emconf.php
+++ asdis/trunk/ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'AOE GmbH',
-	'version' => '0.0.5',
+	'version' => '0.0.6',
 	'constraints' => array(
 		'depends' => array(
 		),
