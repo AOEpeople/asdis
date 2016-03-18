2f1e2920a5b0d8c1df42f0dfcb887cae83fcc720
Kevin Schu <kevin.schu@aoe.com>
2016-03-18 13:52:10 +0100

    [FEATURE] added scraper for data-src attributes

---
+++ Tests/Content/Scraper/Html/DataSrcTest.php
@@ -0,0 +1,47 @@
+<?php
+
+$asdisBaseDir = dirname(__FILE__) . '/../../../../';
+require_once $asdisBaseDir . 'Tests/AbstractTestcase.php';
+require_once $asdisBaseDir . 'Classes/Content/Scraper/Html/DataSrc.php';
+
+/**
+ * Tx_Asdis_Content_Scraper_Html_Image tests.
+ */
+class Tx_Asdis_Content_Scraper_Html_DataSrcTest extends Tx_Asdis_Tests_AbstractTestcase {
+
+	/**
+	 * @var Tx_Asdis_Content_Scraper_Html_Image
+	 */
+	private $imageScraper;
+
+	/**
+	 * (non-PHPdoc)
+	 * @see PHPUnit_Framework_TestCase::setUp()
+	 */
+	protected function setUp() {
+		$this->imageScraper = new Tx_Asdis_Content_Scraper_Html_DataSrc();
+	}
+
+	/**
+	 * (non-PHPdoc)
+	 * @see PHPUnit_Framework_TestCase::tearDown()
+	 */
+	protected function tearDown() {
+		$this->imageScraper = NULL;
+	}
+
+	/**
+	 * @test
+	 */
+	public function scrape() {
+		$content = '<div data-src="uploads/images/foo.gif" />';
+		$assetFactory = $this->getMock('Tx_Asdis_Domain_Model_Asset_Factory');
+		$assetFactory->expects($this->once())->method('createAssetsFromPaths')->with(array('uploads/tx_templavoila/example.gif'));
+		$attributeExtractor = $this->getMock('Tx_Asdis_Content_Scraper_Extractor_XmlTagAttribute');
+		$attributeExtractor->expects($this->once())->method('getAttributeFromTag')->with('[A-z]?', 'data-src', $content)->will($this->returnValue(array('paths' => array('uploads/tx_templavoila/example.gif'), 'masks' => array('"'))));
+		$this->imageScraper->injectAssetFactory($assetFactory);
+		$this->imageScraper->injectXmlTagAttributeExtractor($attributeExtractor);
+		$this->imageScraper->scrape($content);
+	}
+}
+
