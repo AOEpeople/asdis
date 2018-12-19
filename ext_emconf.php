162e5111636403ecced33b17dadfcd6e71b9495c
Felix Semmler <felix.semmler@gmail.com>
2018-12-19 15:46:32 +0100

    [TASK] Drop support for TYPO3 6.2 (#11)

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
+			'typo3' => '7.6.0-8.7.99',
 		),
 		'conflicts' => array(
 		),
@@ -33,5 +33,3 @@ $EM_CONF[$_EXTKEY] = array(
 	'suggests' => array(
 	),
 );
-
-?>
