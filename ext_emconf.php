5d3f37d3c94906c583a752b4fee4690419117427
Christian Krzeminski <christian.krzeminski@aoe.com>
2019-06-03 09:14:28 +0200

    [TASK] Update min TYPO3 version in ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '2.0.1',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '7.6.0-8.7.99',
+			'typo3' => '7.6.23-8.7.99',
         ],
 		'conflicts' => [
         ],
