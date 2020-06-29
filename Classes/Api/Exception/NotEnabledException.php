46d80ff713f3e458914480724e86cb618d7782b9
Martin Tepper <martin.tepper@aoe.com>
2020-06-29 08:14:38 +0200

    [FIX] correct namespace path to Exception

--- Classes/Api/Exception/NotEnabledException.php
+++ Classes/Api/Exception/NotEnabledException.php
@@ -1,13 +1,13 @@
 <?php
 namespace Aoe\Asdis\Api\Exception;
 
-class NotEnabledException extends Exception
+class NotEnabledException extends \Exception
 {
     /**
      * @param integer $code
-     * @param Exception|null $previous
+     * @param  \Exception|null $previous
      */
-    public function __construct($code = 0, Exception $previous = null)
+    public function __construct($code = 0, \Exception $previous = null)
     {
         parent::__construct('asdis is not enabled at the moment', $code, $previous);
     }
